1a56b87fed61eaa707763c4bddacd08df269e10f
wilber <196733852+wilberdhp@users.noreply.github.com>
2026-01-11 12:49:40 -0500

    update: Agregar un objeto con los valores iniciales para implementar la función de reset

--- src/hooks/useApp.tsx
+++ src/hooks/useApp.tsx
@@ -11,17 +11,30 @@ type Store = {
   setSidebarOpen: (value: boolean) => void
   setIdUser: (value: string | null) => void
   setIsLogged: (value: boolean) => void
+  reset: () => void
 }
 
-const useApp = create<Store>()((set) => ({
+type TypeInitialState = {
+  currentPage: TypeCurrentPage
+  sidebarOpen: boolean
+  idUser: string | null
+  isLogged: boolean
+}
+
+const initialState: TypeInitialState = {
   currentPage: 'dashboard',
   sidebarOpen: false,
   idUser: null,
   isLogged: false,
+}
+
+const useApp = create<Store>()((set) => ({
+  ...initialState,
   setCurrentPage: (value) => set(() => ({ currentPage: value })),
   setSidebarOpen: (value) => set(() => ({ sidebarOpen: value })),
   setIdUser: (value) => set(() => ({ idUser: value })),
   setIsLogged: (value) => set(() => ({ isLogged: value})),
+  reset: () => set({ ...initialState }),
 }))
 
 export default useApp;
