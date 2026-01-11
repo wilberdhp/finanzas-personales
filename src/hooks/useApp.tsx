ebfc3fb7d0b013df1d976ec4011a36d89e649b09
wilber <196733852+wilberdhp@users.noreply.github.com>
2026-01-11 12:48:13 -0500

    update: agregar al estado global idUser, isLogged y funcionalidades para cambiar los valores.

--- src/hooks/useApp.tsx
+++ src/hooks/useApp.tsx
@@ -5,15 +5,23 @@ export type TypeCurrentPage = 'dashboard' | 'incomes' | 'expenses' | 'accounts' 
 type Store = {
   currentPage: TypeCurrentPage
   sidebarOpen: boolean
+  idUser: string | null
+  isLogged: boolean
   setCurrentPage: (value: TypeCurrentPage) => void
   setSidebarOpen: (value: boolean) => void
+  setIdUser: (value: string | null) => void
+  setIsLogged: (value: boolean) => void
 }
 
 const useApp = create<Store>()((set) => ({
   currentPage: 'dashboard',
   sidebarOpen: false,
+  idUser: null,
+  isLogged: false,
   setCurrentPage: (value) => set(() => ({ currentPage: value })),
-  setSidebarOpen: (value) => set(() => ({ sidebarOpen: value }))
+  setSidebarOpen: (value) => set(() => ({ sidebarOpen: value })),
+  setIdUser: (value) => set(() => ({ idUser: value })),
+  setIsLogged: (value) => set(() => ({ isLogged: value})),
 }))
 
 export default useApp;
