56774ec25b2d6e9ef204f46ec167c9d7b4b84ce2
wilber <196733852+wilberdhp@users.noreply.github.com>
2026-01-13 01:13:40 -0500

    update: agregar las nuevas páginas creadas Dashboard, IncomesPage, ExpensesPage, AccountsPage, InvestmentsPage, DebtsPage, SettingsPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import "./App.css";
+import AccountsPage from "./components/Accounts/AccountsPage";
+import Dashboard from "./components/Dashboard/Dashboard";
+import DebtsPage from "./components/Debts/DebtsPage";
+import ExpensesPage from "./components/Expenses/ExpensesPage";
 import Header from "./components/Header/Header";
+import IncomesPage from "./components/Incomes/IncomesPage";
+import InvestmentsPage from "./components/Investments/InvestmentsPage";
+import SettingsPage from "./components/Settings/SettingsPage";
 import Sidebar from "./components/Sidebar";
 import useApp from "./hooks/useApp";
 
 function App() {
 
-  const { sidebarOpen, setSidebarOpen } = useApp()
+  const { currentPage, sidebarOpen, setSidebarOpen } = useApp()
 
   return (
     <div className="flex h-screen bg-gray-100">
@@ -15,7 +22,13 @@ function App() {
         <Header />
 
         <main className="flex-1 overflow-y-auto">
-
+          {currentPage === 'dashboard' && <Dashboard />}
+          {currentPage === 'incomes' && <IncomesPage />}
+          {currentPage === 'expenses' && <ExpensesPage />}
+          {currentPage === 'accounts' && <AccountsPage />}
+          {currentPage === 'investments' && <InvestmentsPage />}
+          {currentPage === 'debts' && <DebtsPage />}
+          {currentPage === 'settings' && <SettingsPage />}
         </main>
       </div>
 
